71303e4ef2ee1f25db1b3949192bab86e5b192c9
Paul <paul@paul-mayer.eu>
2020-10-26 22:44:12 +0100

    test python youtube apis

--- README.md
+++ README.md
@@ -14,3 +14,7 @@ source primo/bin/activate
 ### Tributes
 
 Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
+
+## Reminder
+
+https://pypi.org/project/youtube-transcript-api/
--- test.py
+++ test.py
@@ -1,8 +1,31 @@
-import ffmpeg
+#import ffmpeg
 
-in_file = ffmpeg.input('input.mp4')
+from youtube_search import YoutubeSearch
 
-ffmpeg.concat(
-    in_file.trim(start_frame=10, end_frame=20),
-    in_file.trim(start_frame=30, end_frame=40),
-).drawbox(50, 50, 120, 120, color='red', thickness=5).output('out.mp4').run()
+results = YoutubeSearch('iphone 12', max_results=1).to_dict()
+
+print(results)
+video_id = "cnXapYkboRQ"
+
+from pytube import YouTube 
+  
+try: 
+    #object creation using YouTube which was imported in the beginning 
+    yt = YouTube("https://www.youtube.com/watch?v="+video_id) 
+    print(yt)
+
+    
+except: 
+    print("Connection Error") #to handle exception 
+      
+   
+#from youtube_transcript_api import YouTubeTranscriptApi
+
+#transcript = YouTubeTranscriptApi.get_transcript(video_id)
+#print(transcript)
+#in_file = ffmpeg.input('input.mp4')
+
+# ffmpeg.concat(
+#     in_file.trim(start_frame=10, end_frame=20),
+#     in_file.trim(start_frame=30, end_frame=40),
+# ).drawbox(50, 50, 120, 120, color='red', thickness=5).output('out.mp4').run()
